8e91866af94bf6475760255c31caed29ffa621ea
Ponze96 <101227996+Ponze96@users.noreply.github.com>
2022-03-16 09:16:07 -0300

    Add files via upload

---
+++ Pr-tica-1-_LP-main/Pratica1_Linguagemde Programacao/src/pratica1/Questao4.java
@@ -0,0 +1,21 @@
+package pratica1;
+import java.util.Scanner;
+
+public class Questao4 {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int escolha = 0;
+	do {	
+		System.out.println("1 - Cadastrar Passageiro");
+		System.out.println("2 - Check in");
+		System.out.println("3 - Cancelar Voo");
+		System.out.println("4 - Sair");
+	}
+
+
+}
+
+}
+
+
