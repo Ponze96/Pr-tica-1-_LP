1b25500f37a521d476b4e0be026ce4979d7d65f7
Ponze96 <101227996+Ponze96@users.noreply.github.com>
2022-03-16 23:59:25 -0300

    Add files via upload

---
+++ Pr-tica-1-_LP-main/Pr-tica-1-_LP-main/Pratica1_Linguagemde Programacao/src/pratica1/Questao4.java
@@ -0,0 +1,62 @@
+package pratica1;
+import java.util.Scanner;
+import java.util.Random;
+public class Questao4 {
+	
+	Scanner scan = new Scanner(System.in);
+	String Nome, CPF, dataVoo, horario;
+	int Random;
+	int numeroVoo;
+	int opcao;
+	int escolha = 0;
+	
+	do {	
+		System.out.println("Escolha");
+		System.out.println("1 - Cadastrar Passageiro");
+		System.out.println("2 - Check in");
+		System.out.println("3 - Cancelar Voo");
+		System.out.println("4 - Sair");
+		
+		Scanner scan = new Scanner (System.in);
+		opcao = scan.nextInt();
+		
+		switch(opcao) {
+		case 1:
+				System.out.println("Digite seu nome");
+				Nome = scan.next();
+				System.out.println("Digite seu CPF");
+			    CPF = scan.next();
+				System.out.println(" Digite a data do Voo");
+				dataVoo = scan.next();
+				System.out.println(" Digite o horario");
+				horario = scan.next();
+				System.out.println("Cadastro efetuado com sucesso");
+				
+				Random gerador = new Random();
+				System.out.println(gerador.nextInt(999));
+					
+				break;
+		case 2:
+				System.out.println("Digite seu CPF");	
+				CPF = scan.next();
+		        System.out.println("Digite o numero do Voo");
+				numeroVoo = scan.nextInt();
+				
+				System.out.println("Check in realizado");
+				break;
+		
+		case 3: 
+				System.out.println("Digite seu CPF");
+				CPF = scan.next();		
+				System.out.println("Digite o numero do Voo");
+				numeroVoo = scan.nextInt();
+				System.out.println("Voo cancelado com sucesso");
+						  
+			} while(opcao != -1);
+			
+
+}
+
+}
+
+
--- Pr-tica-1-_LP-main/Pratica1_Linguagemde Programacao/src/pratica1/Questao3.java
+++ Pr-tica-1-_LP-main/Pratica1_Linguagemde Programacao/src/pratica1/Questao3.java
@@ -3,14 +3,8 @@ import java.util.Scanner;
 public class Questao3 {
 
 	public static void main(String[] args) {
-		int count = 0;
-		String sentence = "My name is Pedro, Hello World!! XD";
-		char [] vogais = {'a', 'e', 'i', 'o', 'u'};
-		for (char vogal : vogais)
-			for (char letras : sentence.toLowerCase().toCharArray())
-				if  (letras == vogal) count++;
-		System.out.println("Numero de vogais na setenca e " + count);
-			
+		String frase;
+		
 
 	}
 
